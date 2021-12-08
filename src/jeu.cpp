19e48d35d80971e17b255ab9cbf13a2ca7379f65
WilliamChidiac <94478368+WilliamChidiac@users.noreply.github.com>
2021-12-09 00:20:00 +0100

    Update jeu.cpp

--- src/jeu.cpp
+++ src/jeu.cpp
@@ -3,8 +3,10 @@
 void jeuNormal(SDL_Window *window, SDL_Renderer *renderer1, SDL_Texture *texture1, TTF_Font *font){
 
     //DEF VARIABLES POUR JEU
+    bool AIMODE = false;
     SDL_Rect PLATEAU; //plateau de jeu sur l'interface graphique
     PLATEAU.h = 440; PLATEAU.w = 440; PLATEAU.x = 130; PLATEAU.y = 130; // initialisation des positions
+
     SDL_Rect HIGH, CURRENT;
     HIGH.h = 40; HIGH.y = 40; HIGH.x = 45; HIGH.w = 100;
     CURRENT.h = 40; CURRENT.y = 40; CURRENT.x = 480; CURRENT.w = 70;
@@ -13,6 +15,15 @@ void jeuNormal(SDL_Window *window, SDL_Renderer *renderer1, SDL_Texture *texture
     SDL_Texture *H = SDL_CreateTextureFromSurface(renderer1, high);
     SDL_Texture *C = SDL_CreateTextureFromSurface(renderer1, current);
 
+    SDL_Rect caseAI;
+    caseAI.h = 100; caseAI.w = 100; caseAI.x = 0; caseAI.y = 600;
+    SDL_Rect ai;
+    ai.h = 40; ai.w = 50; ai.x = 25; ai.y = 600;
+    SDL_Rect OnOff;
+    OnOff.h = 55; OnOff.w = 80; OnOff.x = 10; OnOff.y = 640;
+    SDL_Surface *aiT = TTF_RenderText_Blended(font, "AI", {0, 0, 0});
+    SDL_Texture *A = SDL_CreateTextureFromSurface(renderer1, aiT);
+    
     cases GC = init_cases(font, renderer1, 100, 150, 150); //groupement des different rectangle composant les differente les cases du jeu sur l'interface
     plateau P = init_plateau(); //groupementn des differente nombre du plateau
     plateauCases PC = plateauCases(P, GC);
@@ -21,13 +32,6 @@ void jeuNormal(SDL_Window *window, SDL_Renderer *renderer1, SDL_Texture *texture
     bool GAMEISRUNNNING = true;
     int nNum;
     int n4 = 0;
-    for ( auto l:P){
-        for ( auto i:l){
-            if (i == 4){
-                n4++;
-            }
-        }
-    }
     while (GAMEISRUNNNING){
 
         SDL_SetRenderDrawColor(renderer1, 255, 255, 255, SDL_ALPHA_OPAQUE);
@@ -49,8 +53,16 @@ void jeuNormal(SDL_Window *window, SDL_Renderer *renderer1, SDL_Texture *texture
                         break;
                     }else{
                         PC = PC1;
+                        AIMODE = false;
                     }
                     GAMEISRUNNNING = not defaite(PC.first);
+                }
+            }else if (event.type == SDL_MOUSEBUTTONDOWN){
+                int x;
+                int y;
+                SDL_GetMouseState(&x, &y);
+                if((0<x and x<100) and (600<y and 700>y)){
+                    AIMODE = not AIMODE;
                 }
             }else{
                 break;
@@ -68,12 +80,19 @@ void jeuNormal(SDL_Window *window, SDL_Renderer *renderer1, SDL_Texture *texture
             }
 
         }
-        /*
-        if (PC.first != aP){
-            PC.first = ajouteNum(PC.first, nNum);
+        if (AIMODE){
+            PC.first = AI(PC.first, 250, 25);
             PC.second = changeCases(PC);
+            AIMODE = not defaite(PC.first);
+            if (PC.first != aP){
+                nNum = randnum();
+                if (nNum == 4){
+                    n4++;
+                }
+                PC.first = ajouteNum(PC.first, nNum);
+                PC.second = changeCases(PC);
+            }
         }
-        */
         if (not GAMEISRUNNNING){
             continue;
         }
@@ -100,11 +119,32 @@ void jeuNormal(SDL_Window *window, SDL_Renderer *renderer1, SDL_Texture *texture
         SDL_RenderCopy(renderer1, H, NULL, &HIGH);
         SDL_RenderCopy(renderer1, C, NULL, &CURRENT);
 
+        SDL_SetRenderDrawColor(renderer1, 255, 127, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer1, &caseAI);
+        SDL_RenderCopy(renderer1, A, NULL, &ai);
+        if ( not AIMODE){
+            SDL_Surface *On = TTF_RenderText_Blended(font, "ON", {0, 0, 0});
+            SDL_Texture *N = SDL_CreateTextureFromSurface(renderer1, On);
+
+            SDL_RenderCopy(renderer1, N, NULL, &OnOff);
+            SDL_DestroyTexture(N);
+            SDL_FreeSurface(On);
+        }else{
+            SDL_Surface *Off = TTF_RenderText_Blended(font, "OFF", {0, 0, 0});
+            SDL_Texture *F = SDL_CreateTextureFromSurface(renderer1, Off);
+            SDL_RenderCopy(renderer1, F, NULL, &OnOff);
+            SDL_DestroyTexture(F);
+            SDL_FreeSurface(Off);
+
+        }
         affiche_score(scor, renderer1, font);
-        affiche_highscore("score/mode1.txt", renderer1, font, scor);
+        affiche_highscore("score/scoreAI.txt", renderer1, font, scor);
         SDL_RenderPresent(renderer1);
          
     }
+    SDL_DestroyTexture(A);
+    SDL_FreeSurface(aiT);
+
     SDL_DestroyTexture(H);
     SDL_DestroyTexture(C);
     SDL_FreeSurface(high);
